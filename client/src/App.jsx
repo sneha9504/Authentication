ca36dc37172fb36854396d5421637bd43a3f457d
sneha9504 <snehawani4321@gmail.com>
2025-02-02 12:41:50 +0530

    added client and server for authentication

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+function App() {
+  return (
+    <>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
